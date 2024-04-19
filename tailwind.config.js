401358933aec6dce5d0a75ae6d58a7bb1da0100e
Corentin Gitton <corentin.gitton@gmail.com>
2024-04-19 17:56:59 +0100

    feat: disable hover on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
+  future: {
+    hoverOnlyWhenSupported: true,  // Disable hover on mobile
+  },
   theme: {
     extend: {},
   },
